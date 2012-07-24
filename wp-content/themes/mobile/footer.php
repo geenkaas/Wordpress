e1179e5eda26056504c920eb749be73c94e7bead
Firstname LastnameGeen <github@geenkaas.nl>
2012-07-24 11:28:00 +0200

    Updated mobile template

--- wp-content/themes/mobile/footer.php
+++ wp-content/themes/mobile/footer.php
@@ -1,51 +1,21 @@
 	
-			<footer id="footerwrapper">
+	
+			<footer id="footerwrapper" class="alignwrapper">
 				<div class="alignleft">
-					<a href="http://www.nrccharityawards.nl/?device=other">
-						<p>Desktop weergave</p>
+					<a href="<?php echo home_url( '/' ); ?>?device=desktop">
+						<p>
+							<span>Desktop weergave</span>
+							<br />
+							<?php bloginfo('name'); ?> >
+						</p>
 					</a>
 				</div>
-				<div class="alignright" id="iconwrapper">
-					<div id="icon-mail" class="footer-icon">
-						E-mail
-					</div>
-					<div id="newspop">
-						<!-- Begin MailChimp Signup Form -->
-							<h4>Nieuwsbrief</h4>
-							<p>Blijf op de hoogte van de ontwikkelingen rondom de Awards!</p>
-							<form action="http://nrccharityawards.us5.list-manage.com/subscribe/post?u=6ead5bbbc5da8fb730cd353f1&amp;id=26c4b3af50" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-								<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Uw e-mailadres" required>
-								<div class="clear"><input type="submit" value="Aanmelden" name="subscribe" id="aanmeldenbrief" class="button"></div>
-							</form>
-						<!--End mc_embed_signup-->
-					</div>
-					<a href="https://www.facebook.com/NRCCharityAward" target="_blank" id="fb">
-						<div id="icon-facebook" class="footer-icon">
-							Facebook
-						</div>
-					</a>
-					<a href="https://twitter.com/#!/nrccharityaward" target="_blank" id="tw">
-						<div id="icon-twitter" class="footer-icon">
-							Twitter
-						</div>
-					</a>
-				</div>
-						
-						
-				<div id="logofooter">
-					<div id="logo-kennisbank" class="footer-logo">
-						kennisbank
-					</div>
-					<div id="logo-bandbreed" class="footer-logo">
-						bandbreed
-					</div>
-					<div id="logo-ddk" class="footer-logo">
-						ddk
-					</div>
-					<div id="logo-nrcmedia" class="footer-logo">
-						nrcmedia
-					</div>
-				
+				<div class="alignright">
+					<p>
+						&copy; <?php echo date("Y"); ?>
+						<br />
+						Bandbreed
+					</p>
 				</div>
 			</footer>
 		
@@ -54,17 +24,16 @@
 		</div><!-- sitewrapper -->
 		
 		<!-- Load jQuery and custom plugins/scripts -->
-			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-			<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+			<script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
 			<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
 			<script>window.jQuery || document.write('<script src="<?php bloginfo("template_url");?>/scripts/libs/jquery-ui.min.js"><\/script>')</script>
 			<script src="<?php bloginfo("template_url");?>/scripts/script.js"></script>
-	<script src="<?php bloginfo("template_url");?>/scripts/banner.js"></script>
 			
 		<!-- Google Analytics code -->
 			
 		  <script>
-			var _gaq=[["_setAccount","UA-23852114-1"],["_trackPageview"]];
+			var _gaq=[["_setAccount","UA-REPLACE"],["_trackPageview"]];
 			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
 			g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
 			s.parentNode.insertBefore(g,s)}(document,"script"));
