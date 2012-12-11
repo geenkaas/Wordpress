6b66122c341db1c3e450815d2588256cc67e03e3
Firstname LastnameGeen <github@geenkaas.nl>
2012-12-11 15:40:23 +0100

    Updated page and single.php
    reflects changes in buildup: Pages for navigation, custom posts for
    data, posts for blogs.

--- wp-content/themes/geenkaas/page.php
+++ wp-content/themes/geenkaas/page.php
@@ -1,24 +1,38 @@
-				<?php $geenpage = 'page'; 
-				get_header(); ?>
-			
+
+		<?php /* Template Name: Page Default */ ?>
+		<?php get_header(); ?>
 
 			<div id="maincontent" role="main">
-				<div class="paddingwrapper">
-				
-					<article class="postwrapper">
 
-						<?php if(have_posts()) : while(have_posts()) : the_post (); ?>
-						
-							<h3 class="pageheader"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-							<?php the_content();?>
-							
-						<?php endwhile; endif; ?>
-							
+				<?php if ( have_posts() ) : while( have_posts() ) : the_post (); ?>
+
+				<?php $mytags = strip_tags(get_the_tag_list('',',','')); ?>
+
+					<article id="<?php echo $post->post_parent; ?>" class="postwrapper" data-tags="<?php echo $mytags; ?>">
+
+						<h1 class="pageheader">
+							<a href="<?php the_permalink() ?>" rel="bookmark" title="Ga naar <?php the_title_attribute(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</h1>
+
+						<?php the_post_thumbnail('full', array('class' => 'articleimage'));?>
+
+						<?php the_content();?>
+
 					</article>
-					
-				</div>
+
+				<?php endwhile; endif; ?>
+
+				<?php if ( current_user_can('level_10') ) { ?>
+					<div class="level10">
+						page-ontmoet-de-reviewers.php (U bent ingelogd als admin)
+					</div>
+					<div class="<?php echo $post->post_parent; ?>">
+						Parent (actie) ID = <?= $post->post_parent; ?>
+					</div>
+				<?php } ?>
+
 			</div>
-			
-			<p class="phpfilename"><?php global $geenpage; echo $geenpage; ?>.php</p>
-			
-			<?php get_footer(); ?>
+
+		<?php get_footer(); ?>
--- wp-content/themes/geenkaas/single.php
+++ wp-content/themes/geenkaas/single.php
@@ -1,29 +1,28 @@
-		
-			<?php $geenpage = 'single'; ?>
-			<?php get_header(); ?>
-		
+		<?php get_header(); ?>
+
 			<div id="maincontent" role="main">
-					
-				<?php if ( have_posts()) : while(have_posts() ) : the_post ();
-				foreach( (get_the_category()) as $category ) { 
+
+				<?php if ( have_posts()) : while(have_posts() ) : the_post (); ?>
+				<?php $mytags = strip_tags(get_the_tag_list('',',','')); ?>
+				<?php foreach( (get_the_category()) as $category ) {
 					$categories = get_categories();
 				} ?>
-						
-				<article class="postwrapper">
-						
+
+				<article class="postwrapper" data-tags="<?php echo $mytags; ?>">
+
 					<?php the_time('d/m/Y') ?>
-					<h3 class="posttitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-					
+					<h1 class="posttitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
 					<p class="tagwrapper">
 						Tags voor deze post: <?php the_category(' , '); ?>
 					</p>
-					
+
 					<?php the_content(); ?>
-	
+
 					<div id="relatedwrapper">
-						<h4>Gerelateerde artikelen - <?php echo $category->cat_name; ?></h4>
-						
-						<ul>
+						<p class="related">Gerelateerde artikelen - <?php echo $category->cat_name; ?></p>
+
+						<ul class="relatedlist">
 							<?php $args = array(
 								'category_name' => $category->cat_name,
 								'numberposts' => 5,
@@ -32,23 +31,30 @@
 							$lastposts = get_posts($args);
 							$postcount = 0;
 							foreach($lastposts as $post) : setup_postdata($post);  ?>
-							
+
 								<li>
 									<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
 										<?php the_title(); ?>
 									</a>
 								</li>
-							
+
 							<?php $postcount++; endforeach; ?>
 						</ul>
 					</div>
-				
+
 				</article>
-						
+
 				<?php endwhile; endif; ?>
-				
+
+				<?php if ( current_user_can('level_10') ) { ?>
+					<div class="level10">
+						page-ontmoet-de-reviewers.php (U bent ingelogd als admin)
+					</div>
+					<div class="<?php echo $post->post_parent; ?>">
+						Parent (actie) ID = <?= $post->post_parent; ?>
+					</div>
+				<?php } ?>
+
 			</div>
-			
-			<p class="phpfilename"><?php global $geenpage; echo $geenpage; ?>.php</p>
-			
+
 		<?php get_footer(); ?>
